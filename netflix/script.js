5202fea721ee5260c6f266e4059aa452e7e6b795
MAHLOGONOLO <177832004+Mahlogonolo21@users.noreply.github.com>
2026-03-16 00:13:05 +0200

    Update script.js

--- netflix/script.js
+++ netflix/script.js
@@ -2,7 +2,7 @@ const openButton = document.querySelector(".open-btn");
 const closeButton = document.querySelector(".close-btn");
 const navs = document.querySelectorAll(".nav");
 
-openButton.addEventListener("click", () =>
+openButton.addEventListener("click", () => 
   navs.forEach((nav) => nav.classList.add("visible"))
 );
 
